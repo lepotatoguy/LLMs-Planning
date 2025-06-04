fcacf53abca152575a673cae13ab2ee2e45e81be
LePotatoGuy <joyanta.jyoti.mondal@g.bracu.ac.bd>
2025-06-04 00:11:15 -0500

    Wrapping with checker of availability of openai api key, to ensure it is not needed always, considering opensource is used

--- plan-bench/utils/__init__.py
+++ plan-bench/utils/__init__.py
@@ -6,7 +6,9 @@ import hashlib
 from tarski.io import PDDLReader
 from tarski.syntax.formulas import *
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+if "OPENAI_API_KEY" in os.environ:
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    
 random.seed(10)
 
 from .llm_utils import *
